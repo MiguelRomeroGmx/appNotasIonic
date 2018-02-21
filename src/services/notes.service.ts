8f40c01c67f4563474dd1f97db846d0d699421f4
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-02-21 10:59:29 -0600

    Ionic App notas

---
+++ src/services/notes.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class NotesService {
+    notes =[
+    {id:1, title:'Nota 1', description:'Descripción de nota 1'},
+    {id:2, title:'Nota 2', description:'Descripción de nota 2'},
+    {id:3, title:'Nota 3', description:'Descripción de nota 3'}
+  ];
+
+  /**
+   * getNotes
+   */
+  public getNotes() {
+      return this.notes;
+  }
+
+  /**
+   * getNote
+id    */
+  public getNote(id) {
+      return this.notes.filter(function (e, i) {
+          return e.id == id  
+      }) [0] || {id: null, title: null, description: null};
+  }
+
+  /**
+   * createNote
+   */
+  public createNote(note) {
+      this.notes.push(note);
+  }
+
+   public editNote(note) {
+      for (let i; this.notes.length; i++){
+           if (this.notes[i] == note) {
+               this.notes[i] = note;
+           }     
+      }
+  }
+
+}
